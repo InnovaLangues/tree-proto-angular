9c2e02cb469fdd86c8baaf213ac068018a49392b
Louis <louis@koala2>
2013-08-14 14:41:08 +0200

    tooltips

---
+++ app/js/script.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function($) {
+  /*$("body").on('load','.sidebar',function(){
+    $(".sidebar").pageslide({ direction: "left", modal: true });
+  });*/
+  $(".sidebar").on('click', function(){
+    console.log("boop");
+    // $(this).pageslide({ direction: "left", modal: true });
+  });
+});
+
+
